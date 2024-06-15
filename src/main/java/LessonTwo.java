d8d5f0144066111207383d52cc484f3214b48bbe
SergeiErm <ermakovs902@gmail.com>
2024-06-15 16:03:25 +0300

    second commit

---
+++ src/main/java/LessonTwo.java
@@ -0,0 +1,66 @@
+public class LessonTwo {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 25;
+        int c = 5;
+        int result = a + b + c;
+        System.out.println(result);
+    }
+     static {
+         int a = 10;
+         int b = 25;
+         int c = 5;
+         int d = a + b;
+         int result = d - c;
+         System.out.println(result);
+     }
+
+      static {
+          int a = 7;
+          int b = 8;
+          int result = a * b;
+          System.out.println(result);
+      }
+
+       static {
+           int a = 14;
+           int b = 3;
+           double result = a / b;
+           System.out.println(result);
+       }
+        static {
+            int a = 5;
+            int b = 2;
+            int result = a % b;
+            System.out.println(result);
+        }
+         static {
+             int a = 4;
+             int b = 5;
+             int c = 3;
+             int d = 2;
+             int result = (((a + b) / c) * (b + c)) / d;
+             System.out.println(result);
+         }
+          static{
+              int a = 7;
+              int b = 8;
+              int c = 9;
+              int d = 3;
+              int g = 4;
+              int result = (((a*b/c)*(b+c)/d)*(a+g*b)/d);
+              System.out.println(result);
+          }
+}
+
+
+
+
+
+
+
+
+
+
+
