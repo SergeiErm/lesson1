873c2392b7d770ab55a998b66459919ca6ae76af
SergeiErm <ermakovs902@gmail.com>
2024-07-03 17:39:55 +0300

    Improvements

---
+++ src/main/java/by/devTech/secondStep/lessonSeven/Task4.java
@@ -0,0 +1,2 @@
+package by.devTech.secondStep.lessonSeven;public class Task4 {
+}
--- src/main/java/by/devTech/secondStep/lessonSix/TaskThree.java
+++ src/main/java/by/devTech/secondStep/lessonSix/TaskThree.java
@@ -4,13 +4,12 @@ public class TaskThree {
 
     public static void main(String[] args) {
 
-        int a = -99;
-        int b = 299;
-        int c = a % 3;
-        if (c != 0) a += 3 - c;
-        int d = 0;
-        for (int i = a; i <= b; i++) d += i;
-        System.out.println(d);
-
+        int sum = 0;
+        for (int i = -99; i <= 299; i++) {
+            if (i % 3 == 0){
+                sum += i;
+            }
+        }
+        System.out.println(sum);
     }
 }
